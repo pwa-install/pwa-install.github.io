13bd7ecbf8175aa0f449b0519df2b3ddbf09e249
jackson-cmd <3470341918@qq.com>
2025-02-06 15:16:03 -0600

    Add LLM

--- LLM/sw.js
+++ LLM/sw.js
@@ -1,23 +1,23 @@
 console.log('Service Worker 注册成功');
-// import { ServiceWorkerMLCEngineHandler } from 'https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm';
+importScripts('https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm');
 
-// let handler;
+let handler;
 
-// self.addEventListener('activate', (event) => {
-//   handler = new ServiceWorkerMLCEngineHandler();
-//   console.log('Service Worker 已激活');
-// });
+self.addEventListener('activate', (event) => {
+  handler = new ServiceWorkerMLCEngineHandler();
+  console.log('Service Worker 已激活');
+});
 
-// self.addEventListener('message', async (event) => {
-//   if (!handler) return;
+self.addEventListener('message', async (event) => {
+  if (!handler) return;
 
-//   if (event.data.type === 'query') {
-//     try {
-//       const response = await handler.handle(event.data.text);
-//       event.source.postMessage({ response });
-//     } catch (error) {
-//       console.error('WebLLM 处理错误:', error);
-//       event.source.postMessage({ error: '处理失败' });
-//     }
-//   }
-// });
+  if (event.data.type === 'query') {
+    try {
+      const response = await handler.handle(event.data.text);
+      event.source.postMessage({ response });
+    } catch (error) {
+      console.error('WebLLM 处理错误:', error);
+      event.source.postMessage({ error: '处理失败' });
+    }
+  }
+});
